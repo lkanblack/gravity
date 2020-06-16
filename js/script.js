574ac977949a396328d4f7ef8a1dc00b888c83f2
Andrei <gravity@gravity.com>
2020-06-16 12:25:53 +0300

    tabs update

--- js/script.js
+++ js/script.js
@@ -17,5 +17,13 @@ $(function(){
                 items:1
             }
         }
-    })
+    });
+          
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+        e.preventDefault();
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
 });
+
