13f999168df631e4f593433efc6351256abd559e
Andrei <gravity@gravity.com>
2020-06-17 14:53:25 +0300

    2 screen adaptive

--- js/script.js
+++ js/script.js
@@ -18,6 +18,26 @@ $(function(){
             }
         }
     });
+
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        navText: ['<img src="img/left.svg">','<img src="img/right.svg">'],
+        startPosition: 1,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
           
     $('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
         e.preventDefault();
